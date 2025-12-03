41d5f50bd110e75c9d9a11a0a56e473595552c1b
Lucas Fernando <ferlucas074@gmail.com>
2025-12-03 00:25:05 -0300

    Botão Next criado em Js

---
+++ scripty.js
@@ -0,0 +1,14 @@
+let preButton = document.getElementById('prev');
+let nextButton = document.getElementById('next');
+let container = document.querySelector('.container');
+let items = document.querySelectorAll('.list .item');
+let indixator = document.querySelectorAll('.indicators button');
+let dots = indicator.querySelectorAll('ul li');
+
+nextButton.onclick = () => {
+    console.log('Botão next');
+}
+
+preButton.onclick = () => {
+
+}
